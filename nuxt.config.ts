c00d01a8e5e99656df8d6b2c19aec754adfdb9dc
yamagishi daichi <d-yamagishi@gstaff.co.jp>
2021-02-09 21:10:12 +0900

    環境変数設定変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,14 +16,14 @@ const config: NuxtConfig = {
   },
 
   env: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || 'AIzaSyDxsHJQCuXTT8Zeb4z3XKKUg4z6VDbOCQA',
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || 'garbage-removal-notifica-8643c.firebaseapp.com',
-    FIREBASE_URL: process.env.FIREBASE_URL || 'https://garbage-removal-notifica-8643c.firebaseio.com',
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || 'garbage-removal-notifica-8643c',
-    FIREBASE_STRAGE_BUCKET: process.env.FIREBASE_STRAGE_BUCKET || 'garbage-removal-notifica-8643c.appspot.com',
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID || '68672887439',
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID || '1:68672887439:web:6dfce8870e97d0d7606efa',
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID || 'G-1YWWHJ9QZK'
+    firebaseApiKey: process.env.FIREBASE_API_KEY as string,
+    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
+    firebaseUrl: process.env.FIREBASE_URL as string,
+    firebaseProjectID: process.env.FIREBASE_PROJECT_ID as string,
+    firebaseStrageBucket: process.env.FIREBASE_STRAGE_BUCKET as string,
+    firebaseMessagingSenderID: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
+    firebaseAppID: process.env.FIREBASE_APP_ID as string,
+    firebaseMeasurementID: process.env.FIREBASE_MEASUREMENT_ID as string
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
