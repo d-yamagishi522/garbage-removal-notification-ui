37eb9b9b632552045a7637b969be84ff3b2b1157
yamagishi daichi <d-yamagishi@gstaff.co.jp>
2021-03-01 18:42:11 +0900

    console追加

--- plugins/liff.ts
+++ plugins/liff.ts
@@ -1,7 +1,9 @@
 import liff from '@line/liff'
 
+console.log('koko')
 if (!liff.id) {
   const liffId: string = process.env.liffId as string
+  console.log('liffId', liffId)
   // liffの初期化
   liff.init({ liffId })
 }
