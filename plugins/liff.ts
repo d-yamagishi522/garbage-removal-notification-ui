4e6d1cfccb2db818eeefb8f9d5e892345fe996d7
yamagishi daichi <d-yamagishi@gstaff.co.jp>
2021-03-01 18:56:40 +0900

    heorku.ymlに環境変数追加

--- plugins/liff.ts
+++ plugins/liff.ts
@@ -1,9 +1,7 @@
 import liff from '@line/liff'
 
-console.log('koko')
 if (!liff.id) {
   const liffId: string = process.env.liffId as string
-  console.log('liffId', liffId)
   // liffの初期化
   liff.init({ liffId })
 }
